d491a87f13e2ef5ce26779aedd96d6db7c4355f6
Maelus321 <mael.sau@hotmail.com>
2026-01-13 15:12:17 +0100

    Create repo

---
+++ star-wars/src/Controller/ItemsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class ItemsController extends AbstractController{
+
+    public function create(): Response {
+        return new Response("");
+    }
+    public function read(): Response {
+        return new Response("");
+    }
+    public function update(): Response {
+        return new Response("");
+    }
+    public function delete(): Response {
+        return new Response("");
+    }
+}
